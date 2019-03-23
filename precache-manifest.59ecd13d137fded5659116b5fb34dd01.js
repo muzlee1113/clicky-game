37df78bb6218b0181dab1c936914dba79d4cc08f
Yuwen Li <muzlee1113@gmail.com>
2019-03-23 13:00:20 -0500

    Updates

--- precache-manifest.59ecd13d137fded5659116b5fb34dd01.js
+++ precache-manifest.59ecd13d137fded5659116b5fb34dd01.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.895984e4.js"
   },
   {
-    "revision": "6cf40b02b7e4032f98b0",
-    "url": "/clicky-game/static/js/main.2d387962.chunk.js"
+    "revision": "c1a9317fbdf3fee44270",
+    "url": "/clicky-game/static/js/main.1a478493.chunk.js"
   },
   {
     "revision": "19d95efcc4ebcd21d2b5",
     "url": "/clicky-game/static/js/2.fd9fc717.chunk.js"
   },
   {
-    "revision": "6cf40b02b7e4032f98b0",
+    "revision": "c1a9317fbdf3fee44270",
     "url": "/clicky-game/static/css/main.e61a3c3b.chunk.css"
   },
   {
-    "revision": "765dabb8bafd2a55c538f5e7a154b33b",
+    "revision": "33f0a9e8ffd433b3fc379fec15109ce6",
     "url": "/clicky-game/index.html"
   }
 ];
