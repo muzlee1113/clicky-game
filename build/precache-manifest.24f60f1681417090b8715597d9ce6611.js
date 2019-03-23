3b6a293e62f2174825bf4a06420af5e29a34d0a4
Yuwen Li <muzlee1113@gmail.com>
2019-03-23 12:17:39 -0500

    deploy to the right branch

---
+++ build/precache-manifest.24f60f1681417090b8715597d9ce6611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "6cf40b02b7e4032f98b0",
+    "url": "/clicky-game/static/js/main.2d387962.chunk.js"
+  },
+  {
+    "revision": "19d95efcc4ebcd21d2b5",
+    "url": "/clicky-game/static/js/2.fd9fc717.chunk.js"
+  },
+  {
+    "revision": "6cf40b02b7e4032f98b0",
+    "url": "/clicky-game/static/css/main.e61a3c3b.chunk.css"
+  },
+  {
+    "revision": "765dabb8bafd2a55c538f5e7a154b33b",
+    "url": "/clicky-game/index.html"
+  }
+];
